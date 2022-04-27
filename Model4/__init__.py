7335617d9c23c2b35b919cb3108416efcb1393fd
MontyMinh <100120146+MontyMinh@users.noreply.github.com>
2022-04-27 12:42:11 +0700

    Create __init__.py

---
+++ Model4/__init__.py
@@ -0,0 +1,9 @@
+"""
+This folder contains the Python code of Model 4. It contains 4 files:
+
+- preprocessing.py, file for collecing the data to run the optimization from excel sheets.
+- optimization.py, file for setting up the linear program and optimize.
+- postprocessing.py, file for arranging the output data back into Excel.
+- ui.py, file for hosting the user interface for interacting with the full optimization program.
+
+"""
