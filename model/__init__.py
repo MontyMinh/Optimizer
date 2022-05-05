58aa1b66672a0de0e57aae24a5968d80ffcd8620
Minh <monty.minh@DeheusVietNam.com>
2022-05-05 17:24:55 +0700

    Updata __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -4,6 +4,8 @@ This folder contains the Python code of Model 4. It contains 4 files:
 - preprocessing.py, file for collecing the data to run the optimization from excel sheets.
 - optimization.py, file for setting up the linear program and optimize.
 - postprocessing.py, file for arranging the output data back into Excel.
+- program.py, file for running the entire program from start to finish.
+- modeldata.py, file for storing all the important program data.
 - ui.py, file for hosting the user interface for interacting with the full optimization program.
 
 """
